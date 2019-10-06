bbb698a0c168dcf7f394d0eeb67822ee8f9c8e23
rockangator <32011655+rockangator@users.noreply.github.com>
2019-10-06 12:31:11 +0530

    Add files via upload

---
+++ nmcu_mqtt_sub.ino
@@ -0,0 +1,123 @@
+
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+// Update these with values suitable for your network.
+
+const char* ssid = "GREAT44";
+const char* password = "qwertyuiop";
+const char* mqtt_server = "192.168.43.217";
+
+const char* mqttUser = "username";
+const char* mqttPassword = "qwertyuiop";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup() {
+  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Switch on the LED if an 1 was received as first character
+  if ((char)payload[0] == 'U') {
+    Serial.print("Up");
+    up();
+    digitalWrite(BUILTIN_LED, HIGH);
+  } 
+  else if ((char)payload[0] == 'D') {
+    Serial.print("Down");
+    down();
+  } 
+  else if ((char)payload[0] == 'L') {
+    Serial.print("Left");
+    left();
+  }
+  else if ((char)payload[0] == 'R') {
+    Serial.print("Right");
+    right();
+  } else {
+    digitalWrite(BUILTIN_LED, LOW);  // Turn the LED off by making the voltage HIGH
+  }
+
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    if (client.connect("ESP8266Client11",mqttUser, mqttPassword)) {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      //client.publish("outTopic", "hello world");
+      // ... and resubscribe
+      client.subscribe("test");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop() {
+
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+}
+
+void up(){
+  //digitalWrite();
+  Serial.print("UpUpUp");
+}
+
+void down(){}
+
+void left(){}
+
+void right(){}
+
